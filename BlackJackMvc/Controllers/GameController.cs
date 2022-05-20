93f7f121f89c0389035388f507fedccf73af1682
Alex Ivimey <alexivimey@gmail.com>
2022-05-20 11:08:22 -0300

    removed usings

--- BlackJackMvc/Controllers/GameController.cs
+++ BlackJackMvc/Controllers/GameController.cs
@@ -1,10 +1,6 @@
 ﻿using BlackJackMvc.Models;
 using BlackJackMvc.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BlackJackMvc.Controllers
 {
